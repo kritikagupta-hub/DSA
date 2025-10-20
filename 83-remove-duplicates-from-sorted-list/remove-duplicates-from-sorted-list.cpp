ad3de0f1d003668a8bbce5747ea38129268ab9e5
kritikagupta-hub <kritikagupta16102005@gmail.com>
2025-10-20 10:55:49 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (11.41%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -11,18 +11,20 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
+        //emty list
         if(head==NULL){
             return NULL;
         }
         ListNode*curr=head;
         while(curr!=NULL){
+            //if two adjacent nodes data is same then we have to delete one node
             if((curr->next!=NULL)&& curr->val== curr->next->val){
                 ListNode*next_next= curr->next->next;
                 ListNode*nodeTOdelete=curr->next;
                 delete(nodeTOdelete);
                 curr->next=next_next;
              }
-            else{
+            else{//not equal
                 curr=curr->next;
             }
         }

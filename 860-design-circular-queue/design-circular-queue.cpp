0c427457b3af89952ffc0c8d662526670982a146
kritikagupta-hub <kritikagupta16102005@gmail.com>
2025-11-08 11:20:39 +0530

    Time: 1 ms (75.17%) | Memory: 23.5 MB (80.67%) - LeetSync

---
+++ 860-design-circular-queue/design-circular-queue.cpp
@@ -0,0 +1,78 @@
+
+
+class MyCircularQueue {
+    int* arr;
+    int front;
+    int rear;
+    int size;
+public:
+    MyCircularQueue(int k) {
+        size = k;
+        arr = new int[size];
+        front = rear = -1;
+    }
+    
+    bool enQueue(int value) {
+        if (isFull()) {
+            return false; // Queue full
+        }
+        else if (isEmpty()) {
+            front = rear = 0;
+        }
+        else if (rear == size - 1 && front != 0) {
+            rear = 0; // wrap around
+        }
+        else {
+            rear++;
+        }
+        arr[rear] = value;
+        return true;
+    }
+    
+    bool deQueue() {
+        if (isEmpty()) {
+            return false; // Queue empty
+        }
+        arr[front] = -1;
+        if (front == rear) {
+            // Only one element was there
+            front = rear = -1;
+        }
+        else if (front == size - 1) {
+            front = 0; // wrap around
+        }
+        else {
+            front++;
+        }
+        return true;
+    }
+    
+    int Front() {
+        if (isEmpty()) return -1;
+        return arr[front];
+    }
+    
+    int Rear() {
+        if (isEmpty()) return -1;
+        return arr[rear];
+    }
+    
+    bool isEmpty() {
+        return (front == -1);
+    }
+    
+    bool isFull() {
+        return ((front == 0 && rear == size - 1) || (rear + 1) % size == front);
+    }
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */

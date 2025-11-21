1cdb7914d044db43361175b8704114887ed53fac
kritikagupta-hub <kritikagupta16102005@gmail.com>
2025-11-21 15:00:35 +0530

    Time: 75 ms (56.07%) | Memory: 98.6 MB (51.67%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -4,9 +4,12 @@ public:
         unordered_map<int , int> a;
         for(int i =0;i<nums.size();i++){
             int val = nums[i];
+            // sliding window concept 
+            // if jo val h vo already map m h that means duplicate h to true return hoga and phir check hoga ki kya range choti ho gai k se agr choti ho gai to oviously duplicate h usme return true 
             if( a.find(val) != a.end() && i-a[val] <= k){
                 return true;
             }
+            // agr nhi h to uss value ko map m dall do
             a[val] = i;
         }
         return false;

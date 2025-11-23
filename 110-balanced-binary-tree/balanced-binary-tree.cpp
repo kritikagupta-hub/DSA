13a1c7a37e6cc5299e09c38af9b8b9be698d3db8
kritikagupta-hub <kritikagupta16102005@gmail.com>
2025-11-23 17:42:35 +0530

    Time: 0 ms (100.00%) | Memory: 23.1 MB (56.82%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<bool,int> isBalancedFast(TreeNode* root){
+        if(root == NULL){
+            pair<bool,int> p = make_pair(true,0);
+            return p;
+        }
+        pair<bool,int> left = isBalancedFast(root->left);
+        pair<bool,int> right = isBalancedFast(root->right);
+
+        bool leftAns = left.first;
+        bool rightAns = right.first;
+
+        bool diff = abs ( left.second - right.second) <= 1;
+        pair<bool,int> ans;
+        ans.second= max(left.second , right.second) + 1;
+
+        ans.first = leftAns && rightAns && diff;
+
+        return ans;
+    }
+    bool isBalanced(TreeNode* root) {
+        return isBalancedFast(root).first;
+    }
+};

b235e2a43c41b67b78c6db0b8bc64e67d5d9dd37
kritikagupta-hub <kritikagupta16102005@gmail.com>
2026-01-27 21:30:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sum Tree/sum-tree.cpp
+++ Difficulty: Medium/Sum Tree/sum-tree.cpp
@@ -7,36 +7,44 @@ struct Node
 
 // Should return true if tree is Sum Tree, else false
 class Solution {
-  public:
-    pair<bool,int> isSumTreeFast(Node* root){
-    // base case
-    if(root == NULL){
-        return {true, 0};
+private:
+    pair<bool, int> isSumTreeFast(Node* root) {
+        // Base case: empty tree
+        if (root == NULL) {
+            return {true, 0};
+        }
+
+        // Leaf node
+        if (root->left == NULL && root->right == NULL) {
+            return {true, root->data};
+        }
+
+        // Recursive calls
+        pair<bool, int> leftAns = isSumTreeFast(root->left);
+        pair<bool, int> rightAns = isSumTreeFast(root->right);
+
+        bool isLeftSumTree = leftAns.first;
+        bool isRightSumTree = rightAns.first;
+
+        int leftSum = leftAns.second;
+        int rightSum = rightAns.second;
+
+        bool cond = (root->data == leftSum + rightSum);
+
+        pair<bool, int> ans;
+        if (isLeftSumTree && isRightSumTree && cond) {
+            ans.first = true;
+            ans.second = root->data + leftSum + rightSum;
+        } else {
+            ans.first = false;
+            ans.second = 0;
+        }
+
+        return ans;
     }
 
-    // leaf node: sum should be the leaf's value
-    if(root->left == NULL && root->right == NULL){
-        return {true, root->data};
-    }
-    
-    pair<bool,int> leftAns = isSumTreeFast(root->left);
-    pair<bool,int> rightAns = isSumTreeFast(root->right);
-    
-    bool left = leftAns.first;
-    bool right = rightAns.first;
-    
-    bool combo = (root->data == leftAns.second + rightAns.second);
-    
-    if(left && right && combo){
-        return {true, root->data + leftAns.second + rightAns.second};
-    }
-    else{
-        return {false, 0};
-    }
-}
-
+public:
     bool isSumTree(Node* root) {
-        // Your code here
         return isSumTreeFast(root).first;
     }
 };

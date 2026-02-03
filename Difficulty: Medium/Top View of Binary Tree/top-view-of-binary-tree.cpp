fda8ccb0a4287716720922a1337b029e1ccfbd20
kritikagupta-hub <kritikagupta16102005@gmail.com>
2026-02-03 16:00:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -0,0 +1,50 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> topView(Node *root) {
+        // code here
+        vector<int> ans;
+        if (root == NULL){
+            return ans;
+        }
+        map<int,int> topNode;
+        queue<pair<Node*, int> >q;
+        
+        q.push(make_pair(root,0));
+        while(!q.empty()){
+            pair<Node*,int> temp = q.front();
+            q.pop();
+            Node*frontNode = temp.first;
+            int hd = temp.second;
+            
+            // only one value of hd if another value comes then it means it overlap 
+            if(topNode.find(hd)== topNode.end()){
+                topNode[hd] = frontNode->data;
+            }
+            if(frontNode->left){
+                q.push(make_pair(frontNode->left,hd-1));
+            }
+            if(frontNode->right){
+                q.push(make_pair(frontNode->right,hd+1));
+            }
+        }
+        for(auto i: topNode){
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};

8540094326ceb52b0d9099b70f409ec9c31873fd
kritikagupta-hub <kritikagupta16102005@gmail.com>
2026-03-03 17:47:21 +0530

    Time: 103 ms (44.17%) | Memory: 174.6 MB (17.09%) - LeetSync

---
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Box {
+public:
+    int min, max, sum;
+    bool isBST;
+    Box() {
+        min = INT_MAX;
+        max = INT_MIN;
+        sum = 0;
+        isBST = 1;
+    }
+};
+class Solution {
+public:
+    Box* maxSum(TreeNode* root, int& totalSum) {
+        if (!root)
+            return new Box();
+        else {
+            Box* lefthead = maxSum(root->left, totalSum);
+            Box* righthead = maxSum(root->right, totalSum);
+            if (lefthead->isBST && righthead->isBST &&
+                lefthead->max < root->val && righthead->min > root->val) {
+                Box* head = new Box();
+                head->sum = root->val + lefthead->sum + righthead->sum;
+                head->min = min(root->val, lefthead->min);
+                head->max = max(root->val, righthead->max);
+                totalSum = max(totalSum, head->sum);
+                return head;
+            } else {
+                Box* curr = new Box();
+                curr->isBST = 0;
+                return curr;
+            }
+        }
+    }
+    int maxSumBST(TreeNode* root) {
+        int totalSum = 0;
+        maxSum(root, totalSum);
+        return totalSum;
+    }
+};

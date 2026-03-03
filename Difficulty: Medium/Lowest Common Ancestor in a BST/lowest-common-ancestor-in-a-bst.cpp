e1c35db1c1a2b6c8536ea48fff6b1f184d857c9c
kritikagupta-hub <kritikagupta16102005@gmail.com>
2026-03-03 17:25:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.cpp
+++ Difficulty: Medium/Lowest Common Ancestor in a BST/lowest-common-ancestor-in-a-bst.cpp
@@ -17,31 +17,17 @@ class Solution {
   public:
     Node* LCA(Node* root, Node* n1, Node* n2) {
         // code here
-        //if no is n1 and n2 it is null 
-        if(root == NULL){
-            return NULL;
-        }
-        // if n1 or n2 mill gy then return root
-        if(root == n1 || root== n2){
-            return root;
-        }
-        // Traverse--
-        Node* leftAns = LCA(root->left,n1,n2);
-        Node* rightAns= LCA(root->right,n1,n2);
-        // if left se ans mila and right se bhi nila then that is ans
-        if(leftAns != NULL && rightAns != NULL){
-            return root;
-        }
-        // if any one is non null then return that side
-        else if(leftAns != NULL && rightAns == NULL){
-            return leftAns;
-        }
-        else if(leftAns == NULL && rightAns != NULL){
-            return rightAns;
-        }
-        else{
-            return NULL;
-        }
+       // base case
+       if(root == NULL){
+           return NULL;
+       }
+       if(root->data < n1->data && root->data < n2->data){
+           return LCA(root->right, n1,n2);
+       }
+       if(root->data > n1->data && root->data > n2->data){
+           return LCA(root->left, n1,n2);
+       }
+       return root;
         
     }
 };

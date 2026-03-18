4deded8bf2f2abdd0ebbe90f265fc74223e55093
kritikagupta-hub <kritikagupta16102005@gmail.com>
2026-03-18 12:39:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Distribute Candies/distribute-candies.py
@@ -0,0 +1,24 @@
+'''
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.right = None
+        self.left = None
+'''
+
+class Solution:
+    def distCandy(self, root):
+        ret=0
+        def dfs(cur=root):
+            nonlocal ret
+            if not cur:
+                return 0,0
+            la,lb=dfs(cur.left)
+            ra,rb=dfs(cur.right)
+            candycnt=la+ra+cur.data
+            nodecnt=lb+rb+1
+            ret+=abs(la-lb)
+            ret+=abs(ra-rb)
+            return candycnt,nodecnt
+        dfs()
+        return ret

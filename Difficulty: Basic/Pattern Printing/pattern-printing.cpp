4e62e91469f8d55319c9663d7a11356fa194f658
kritikagupta-hub <kritikagupta16102005@gmail.com>
2026-03-25 11:49:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Pattern Printing/pattern-printing.cpp
@@ -0,0 +1,15 @@
+// User function Template for C++
+class Solution {
+  public:
+    void printPattern(int N) {
+        // Write Your Code here
+        for(int i=1;i<=N;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                cout<<"*";
+            }
+            cout<<" ";
+        }
+    }
+};

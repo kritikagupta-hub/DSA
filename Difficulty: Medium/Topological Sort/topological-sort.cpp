cf4db1095dafc6b01b5e366746004949b78f01c1
kritikagupta-hub <kritikagupta16102005@gmail.com>
2026-03-26 11:25:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.cpp
+++ Difficulty: Medium/Topological Sort/topological-sort.cpp
@@ -1,47 +1,47 @@
 class Solution {
-public:
-    void tpSort(int node, vector<bool> &visited, stack<int> &s,
-                unordered_map<int, list<int>> &adj) {
-        
-        visited[node] = true;
-        
-        for (auto neighbour : adj[node]) {
-            if (!visited[neighbour]) {
-                tpSort(neighbour, visited, s, adj);
-            }
-        }
-        
-        s.push(node);
-    }
-
-    vector<int> topoSort(int V, vector<vector<int>> &edges) {
-        
-        int e = edges.size();  // alculate here
-        
-        // create adjacency list
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        //creaye adjacency list
         unordered_map<int, list<int>> adj;
-        for (int i = 0; i < e; i++) {
+        for (int i = 0; i < edges.size(); i++) {
             int u = edges[i][0];
             int v = edges[i][1];
             adj[u].push_back(v);
         }
-
-        vector<bool> visited(V, false);
-        stack<int> s;
-
-        // call DFS for all components
-        for (int i = 0; i < V; i++) {
-            if (!visited[i]) {
-                tpSort(i, visited, s, adj);
+        //find all indegres
+        vector<int> indegree(V,0);
+        for(auto i:adj){
+            for(auto j:i.second){
+                indegree[j]++;
             }
         }
-
-        vector<int> ans;
-        while (!s.empty()) {
-            ans.push_back(s.top());
-            s.pop();
+        //0 indegree walo ko push kardo
+        queue<int> q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
         }
-
+        //do bfs
+        vector<int> ans;
+        while(!q.empty()){
+            int front=q.front();
+            q.pop();
+            //ans store
+            ans.push_back(front);
+            
+            
+            //neighbour indegree update
+            for(auto neighbour:adj[front]){
+                indegree[neighbour]--;
+                if(indegree[neighbour]==0){
+                    q.push(neighbour);
+                }
+            }
+            
+        }
         return ans;
+        
     }
 };

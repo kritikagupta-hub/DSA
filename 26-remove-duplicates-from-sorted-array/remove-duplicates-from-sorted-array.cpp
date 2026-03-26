4b8840f552aa0d8000cc8a408a036d78e688927c
kritikagupta-hub <kritikagupta16102005@gmail.com>
2026-03-26 18:33:49 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (18.17%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -4,8 +4,11 @@ public:
         if(nums.size()==0){
             return 0;
         }
+        //first unique elemnt in j 
         int j=1;
         for(int i=1;i<nums.size();i++){
+            //as we know that array is sorted so same elemnt are at adjacent we will check
+            //if j-1!=i then put it into unique list;
             if(nums[i]!=nums[j-1]){
                 nums[j]=nums[i];
                 j++;

cbcecb6788d74b8ab6074d48b9b91819fe2d231a
kritikagupta-hub <kritikagupta16102005@gmail.com>
2026-03-26 16:49:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,50 +1,49 @@
 class Solution {
   public:
-  
-    bool dfs(int node, vector<int> adj[], vector<int> &visited, vector<int> &dfsVisited) {
-        visited[node] = 1;
-        dfsVisited[node] = 1;
-        
-        for(auto neighbour : adj[node]) {
-            
-            // if not visited
-            if(!visited[neighbour]) {
-                if(dfs(neighbour, adj, visited, dfsVisited)) {
-                    return true;
-                }
-            }
-            // if already in DFS stack → cycle
-            else if(dfsVisited[neighbour]) {
-                return true;
-            }
-        }
-        
-        dfsVisited[node] = 0; // backtrack
-        return false;
-    }
-  
     bool isCyclic(int V, vector<vector<int>> &edges) {
-        
-        // create adjacency list
-        vector<int> adj[V];
-        for(auto edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
+        // code here
+        unordered_map<int,list<int>>adj;
+        for(int i=0;i<edges.size();i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
             adj[u].push_back(v);
         }
         
-        vector<int> visited(V, 0);
-        vector<int> dfsVisited(V, 0);
-        
-        // handle disconnected graph
-        for(int i = 0; i < V; i++) {
-            if(!visited[i]) {
-                if(dfs(i, adj, visited, dfsVisited)) {
-                    return true;
-                }
+        //find all indegeree
+        vector<int> indegree(V,0);
+        for(auto i:adj){
+            for(auto j:i.second){
+                indegree[j]++;
             }
         }
         
-        return false;
+        //0 will push into quque;
+        queue<int> q;
+        for(int i=0;i<V;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        //
+        int count=0;
+        while(!q.empty()){
+            int front=q.front();
+            q.pop();
+            count++;
+        
+        for(auto neighbour:adj[front]){
+            indegree[neighbour]--;
+            if(indegree[neighbour]==0){
+                q.push(neighbour);
+            }
+        }
+        }
+        if(count==V){
+            return false;
+        }
+        else{
+            return true;
+        }
+        
     }
 };

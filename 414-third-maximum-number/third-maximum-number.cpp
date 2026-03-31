5a7387a8c2757072cbdeb98c54ab905a1e21e632
Kritika gupta <kritikagupta16102005@gmail.com>
2026-03-31 12:58:58 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (50.35%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -2,13 +2,24 @@ class Solution {
 public:
     int thirdMax(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        nums.erase(unique(nums.begin(),nums.end()),nums.end()); 
+        int n=nums.size();
+        /*nums.erase(unique(nums.begin(),nums.end()),nums.end()); 
         int n=nums.size();
         if(n>=3){
             return nums[n-3];
         }
         else{
             return nums[n-1];
-        } 
+        } */
+        int count=1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i] != nums[i+1]){
+                count++;
+            }
+            if(count==3){
+                return nums[i];
+            }
+        }
+        return nums[n-1];
     }
 };

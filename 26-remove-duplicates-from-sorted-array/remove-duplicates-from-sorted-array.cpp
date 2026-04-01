ce82220aa899a3ae0441254d59bc3bacc8bef7bf
Kritika gupta <kritikagupta16102005@gmail.com>
2026-04-01 12:10:55 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (5.69%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if(nums.size()==0){
+        /*if(nums.size()==0){
             return 0;
         }
-        //first unique elemnt in j 
         int j=1;
         for(int i=1;i<nums.size();i++){
-            //as we know that array is sorted so same elemnt are at adjacent we will check
-            //if j-1!=i then put it into unique list;
             if(nums[i]!=nums[j-1]){
                 nums[j]=nums[i];
                 j++;
             }
             
         }
-        return j;
+        return j;*/
+        set<int>s;
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
+        }
+        int i=0;
+        for(auto x:s){
+            nums[i]=x;
+            i++;
+        }
+        return s.size();
     }
 };

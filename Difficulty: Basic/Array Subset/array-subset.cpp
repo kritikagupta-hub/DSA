f2927fa1df46331ea2f48e2e2fa3a99f4c3e4363
Kritika gupta <kritikagupta16102005@gmail.com>
2026-04-08 10:44:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Subset/array-subset.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    // Function to check if b is a subset of a
+    bool isSubset(vector<int> &a, vector<int> &b) {
+        // Your code here
+        /*unordered_set<int>s;
+        for(int i=0;i<a.size();i++)
+        {
+            s.insert(a[i]);
+        }
+        for(int i=0;i<b.size();i++){
+            if(s.find(b[i])== s.end()){
+                return false;
+            }
+        }
+        return true;*/
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        int i=0,j=0;
+        while(i<a.size()&&j<b.size()){
+            if(a[i]==b[j]){
+                i++;
+                j++;
+            }
+            else if(a[i] < b[j]){
+                i++;
+            }
+            else{
+                return false;
+            }
+        }
+        return(j==b.size());
+        
+    }
+};

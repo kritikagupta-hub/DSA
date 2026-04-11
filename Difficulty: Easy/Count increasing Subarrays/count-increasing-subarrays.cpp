78c84d71186ac82fa1afdfcb43ffb2edba97747c
Kritika gupta <kritikagupta16102005@gmail.com>
2026-04-11 22:19:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count increasing Subarrays/count-increasing-subarrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    int countIncreasing(vector<int>& arr) {
+        // code here.
+        int ans = 0;
+        for(int i=1,k=1;i<arr.size();i++){
+            if(arr[i]>arr[i-1]){
+                ans+=k++;
+            }
+            else{
+                k=1;
+            }
+        }
+        return ans;
+    }
+};

6ebcd76e4cb5dee9e092ab7f92cdb8fb3b3c524c
Kritika gupta <kritikagupta16102005@gmail.com>
2026-06-08 20:22:56 +0530

    Time: 0 ms (100.00%) | Memory: 32.2 MB (40.25%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
-        int n = nums.size();
-        int minlen = INT_MAX;
-        vector<int>pref(n+1,0);
-        for(int i = 0 ; i < n ; i++){
-            pref[i+1] = pref[i] + nums[i];
-        }
-
-        //use binary search
-        for(int i = 0 ; i < n ;i++){
-            int need = pref[i] + target; // pref[j] - pref[i] >= target
-            int lo = i+1;
-            int hi = n;
-            int ans = -1;
-
-            while(lo <= hi){
-                int mid = (lo + hi)/2;
-
-                if(pref[mid] >= need){
-                    ans = mid;
-                    hi = mid - 1;
-                }
-                else{
-                    lo = mid + 1;
-                }
+        int low=0;
+        int high=0;
+        int res=INT_MAX;
+        int sum=0;
+        int n=nums.size();
+        while(high<n){
+            sum+=nums[high];
+            while(sum>=target){
+                int len = high-low+1;
+                res=min(res,len);
+                sum=sum-nums[low];
+                low++;
             }
-
-            if(ans != -1){
-                minlen = min(minlen, ans - i);
-            }
+            high++;
         }
-        return minlen==INT_MAX ? 0 : minlen;
+        if(res==INT_MAX){
+            return 0;
+        }
+        else{
+            return res;
+        }
     }
 };

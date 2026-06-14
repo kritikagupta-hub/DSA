098ae5c042a28d88bf76c104c1738668cb33049c
Kritika gupta <kritikagupta16102005@gmail.com>
2026-06-14 16:11:16 +0530

    Time: 3 ms (98.26%) | Memory: 11.3 MB (53.84%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -9,28 +9,21 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if (head == NULL || head->next == NULL)
-            return NULL;
+        ListNode * slow=head;
+        ListNode * fast=head;
 
-        ListNode *slow = head;
-        ListNode *fast = head;
-
-        // Step 1: Detect if a cycle exists
-        while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;
-            fast = fast->next->next;
-
-            if (slow == fast) { // meeting point found
-                // Step 2: Find start of the cycle
-                fast = head;
-                while (slow != fast) {
-                    slow = slow->next;
-                    fast = fast->next;
+        while(fast!=NULL && fast->next  != NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
                 }
-                return slow; // cycle start node
+                return slow;
             }
         }
-
-        return NULL; // No cycle
+        return NULL;
     }
 };

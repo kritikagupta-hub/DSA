c903ac997b526bae2fd430ccb5ea70233d1a274f
Kritika Gupta <kritikagupta16102005@gmail.com>
2026-06-25 09:41:46 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (55.15%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,44 +1,22 @@
 class Solution {
 public:
-
-    bool isAlphaNum(char c){
-        if((c >= 'a' && c <= 'z') ||
-           (c >= 'A' && c <= 'Z') ||
-           (c >= '0' && c <= '9')) {
-            return true;
-        }
-        return false;
-    }
-
-    char toLowercase(char c){
-        if(c >= 'A' && c <= 'Z'){
-            return c + ('a' - 'A');
-        }
-        return c; 
-    }
-
     bool isPalindrome(string s) {
-        int start = 0;
-        int end = s.size() - 1;
+        int left=0;
+        int right=s.length()-1;
 
-        while(start <= end){
-
-            while(start < end && !isAlphaNum(s[start])){
-                start++;
+        while(left<right){
+            while(left<right && !isalnum(s[left])){
+                left++;
             }
-
-            while(start < end && !isAlphaNum(s[end])){
-                end--;
+            while(left<right && !isalnum(s[right])){
+                right--;
             }
-
-            if(toLowercase(s[start]) != toLowercase(s[end])){
+            if(tolower(s[left])!=tolower(s[right])){
                 return false;
             }
-
-            start++;
-            end--;
+            left++;
+            right--;
         }
-
         return true;
     }
 };

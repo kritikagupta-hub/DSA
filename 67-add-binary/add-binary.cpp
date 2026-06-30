fa60f1b64c704280901898d194de4582075dc9ca
Kritika Gupta <kritikagupta16102005@gmail.com>
2026-06-30 21:46:49 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (22.49%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,18 +1,19 @@
-// bitset version
 class Solution {
 public:
     string addBinary(string a, string b) {
-        const int l_a=a.length(), l_b=b.length();
-        const int M=max(l_a+1, l_b+1);
-        bitset<10001> A(a), B(b);
-        char C[M+1];
-        C[M]='\0';
-        int carry=0;
-        for(int i=0; i<M; i++){
-            int tmp=A[i]+B[i]+carry;
-            C[M-1-i]='0'+(tmp&1);
-            carry=tmp>1;
+        string res;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0) sum += a[i--] - '0';
+            if(j >= 0) sum += b[j--] - '0';
+            carry = sum > 1 ? 1 : 0;
+            res += to_string(sum % 2);
         }
-        return (C[0]=='0') ? string(C+1):string(C);
+        if(carry) res += to_string(carry);
+        reverse(res.begin(), res.end());
+        return res;
     }
 };

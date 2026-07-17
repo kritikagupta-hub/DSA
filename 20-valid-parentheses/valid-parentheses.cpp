6f19a9ff3e6c42fbfca23c0c5c442b698b0ba005
Kritika Gupta <kritikagupta16102005@gmail.com>
2026-07-17 12:16:55 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (18.49%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -5,14 +5,10 @@ public:
         for(int i=0;i<s.length();i++){
             char ch = s[i];
 
-            // if opening h to stack me push krenge
-            // if closing bracket h to stack ke top ko check krenge and pop krenge
-
             if(ch == '(' || ch == '{' || ch == '['){
                 st.push(ch);
             }
             else{
-                // for closing bracket
                 if(!st.empty()){
                     char top = st.top();
                         if((ch == ')' && top == '(') ||

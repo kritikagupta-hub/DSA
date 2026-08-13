975c054874b0f67aba4be62fe5e6b39a4c033f39
Kritika Gupta <kritikagupta16102005@gmail.com>
2026-08-13 13:00:02 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n <= 2) {
+        /*if(n <= 2) {
             return n;
         }
         int a = 1, b = 2;
@@ -10,6 +10,16 @@ public:
             a = b;
             b = c;
         }
-        return b;
+        return b;*/
+        if(n<=2)
+            return n;
+        
+        vector<int>dp(n+1,0);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };

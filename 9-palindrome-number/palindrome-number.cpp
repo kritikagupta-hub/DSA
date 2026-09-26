43bf6d417909abd5d84298b73a607afb84d44659
Kritika Gupta <kritikagupta16102005@gmail.com>
2026-09-26 21:14:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (35.98%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -4,12 +4,15 @@ public:
         if (x < 0) {
             return false;
         }
+
         long reverse = 0;
         int xcopy = x;
+
         while (x > 0) {
             reverse = (reverse * 10) + (x % 10);
             x /= 10;
         }
+
         return reverse == xcopy;        
     }
 };
